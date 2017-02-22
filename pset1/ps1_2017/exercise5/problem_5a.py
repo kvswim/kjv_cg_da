e8a9f27f56f0adf2580fb98760b82bab610dcdee
MS 600.226 Fall 16 <dsf167@ugradx.cs.jhu.edu>
2017-02-22 01:22:34 -0500

    pset1

--- pset1/ps1_2017/exercise5/problem_5a.py
+++ pset1/ps1_2017/exercise5/problem_5a.py
@@ -1,5 +1,6 @@
 from sklearn import linear_model
 import numpy as np
+import sys
 
 '''read first line of input file'''
 col_names = []
@@ -11,3 +12,15 @@ f.close()
 '''read data from file using genfromtxt'''
 data=np.genfromtxt(filename, delimiter=',', skip_header=1)
 data.shape
+
+'''time to build the model'''
+Y = data[0:150,0]
+X = data[0:150,1:]
+model = linear_model.LogisticRegression(C=1e86)
+fitted_model = model.fit(X,Y)
+beta = fitted_model.coef_
+intercept = fitted_model.intercept_
+testX = data[150:,1:]
+testY = data[150:,0]
+predicted_Y = fitted_model.predict(testX)
+
