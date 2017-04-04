820cd571d41230ff8e8736e6414cb119336bbbf0
MS 600.226 Fall 16 <dsf167@ugrad2.cs.jhu.edu>
2017-04-04 16:03:27 -0400

    pset3 2_2

--- pset3/ex3/3_1.py
+++ pset3/ex3/3_1.py
@@ -1,3 +1,6 @@
+#Kyle Verdeyen
+#Problem Set 3, Exercise 3
+#Computational Genomics: Data Analysis
 #usage: python 3_1.py gmm.csv
 import numpy as np
 import sys
@@ -7,12 +10,17 @@ import matplotlib.pyplot as plt
 from sklearn.mixture import GaussianMixture
 
 def myGMM(data, k, max_iter, conv_tol):
-	gaussmix = GaussianMixture(n_components=k, max_iter=max_iter, reg_covar=conv_tol).fit(data)
+	gaussmix = GaussianMixture(n_components=k, max_iter=max_iter, reg_covar=conv_tol)
+	gaussmix.fit(data)
 	pi = gaussmix.weights_
 	mean = gaussmix.means_
-	z = gaussmix.score_samples
-	print(type(z))
-#assign = max(z[len(data),:])
+	mean=mean.T
+	z = gaussmix.predict_proba(data)
+	n = len(data)-1
+	assign = max(z[n,:])
+	return{'pi':pi, 'mean':mean, 'z':z, 'assign':assign}
 
-input = np.loadtxt(sys.argv[1], skiprows=1, usecols=range(2,11), ndmin=2, delimiter=',')
+
+input = np.loadtxt(sys.argv[1], delimiter=',', ndmin=2, skiprows=1, usecols=range(2, 12))
 gmm = myGMM(input, 3, 100, 1e-6)
+print(gmm['pi'])
