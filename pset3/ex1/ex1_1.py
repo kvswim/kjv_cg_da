c3d94fdc2257294fc9b533102880bfdb11ce9bd7
MS 600.226 Fall 16 <dsf167@ugrad2.cs.jhu.edu>
2017-04-04 00:17:24 -0400

    pset3 1

--- pset3/ex1/ex1_1.py
+++ pset3/ex1/ex1_1.py
@@ -1,9 +1,34 @@
 #Kyle Verdeyen
-#Problem set 3, exercise 1, problem 1
+#Problem set 3, exercise 1
 #Computational Genomics: Data Analysis
 #usage: ex1_1.py genotype_population.csv
 
 import numpy as np
 import sys
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import scale
+from sklearn.decomposition import PCA
 
-gene_pop = np.loadtxt(sys.argv[1], skiprows=1, usecols=range(1,
+#problem 1
+gene_pop = np.loadtxt(sys.argv[1], delimiter=",", skiprows=1, usecols=range(1, 9026),  ndmin=2)
+scale(gene_pop)
+
+#problem 2
+#2a
+pca = PCA(n_components=20)
+pca.fit(gene_pop)
+plt.figure()
+X = pca.components_
+Y = np.dot(X, gene_pop.T)
+plt.title('PCA 2a')
+plt.show()
+#2b
+pca.fit_transform(gene_pop)
+plt.figure()
+X = pca.components_
+Y = np.dot(X, gene_pop.T)
+plt.title('PCA 2b')
+plt.show()
+
